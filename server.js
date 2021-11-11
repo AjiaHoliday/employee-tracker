630c664e17db4e5d6001d340d9c7b07c0a5cd3c0
ajia <ajialeeholiday@gmail.com>
2021-11-10 21:14:59 -0700

    show all menu options functional

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const inquirer = require('inquirer');
+const { menu, newRole, newDept, newEmployeeQ } = require('./lib/questions');
 const {db, getEmployees, getDepartment, getRoles} = require('./db/index');
 require('console.table');
 
-//view all Employees
+function mainMenu() {
+    console.log(`
+    ++__++__++__++__++__++
+    ~EMPLOYEE~TRACKER~
+    ++__++__++__++__++__++`)
+    return inquirer
+    .prompt(menu).then((answer) => {
+        console.clear()
+        switch(answer.menu) {  
+            case "View All Employees":
+               //view all Employees
+                getEmployees().then((employees) => {
+                    console.table(employees)
+                    return mainMenu();
+                }); 
+                break;
+            case "View All Departments":
+                // view all departments
+                getDepartment().then((departments) => {
+                    console.table(departments)
+                    return mainMenu();
+                });
+                break;
+            case "View All Roles":
+                // view all roles
+                getRoles().then((roles) => {
+                    console.table(roles)
+                    return mainMenu();
+                });
+                break;
+        };
+    });
+};
 
-getEmployees().then((employees) => {
-    console.table(employees)
-    return;
-});
-
-// view all departments
-getDepartment().then((departments) => {
-    console.table(departments)
-    return;
-});
-
-// view all roles
-getRoles().then((roles) => {
-    console.table(roles)
-    return;
-});
+mainMenu();
